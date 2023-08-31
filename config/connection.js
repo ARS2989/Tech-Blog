3439392346068450d210af737ea2e379bc432d04
ARS2989 <raysoloriojr@gmail.com>
2023-08-31 01:38:46 -0700

    ok I should stop here

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+const sequelize = process.env.'https://shrouded-sierra-56762-1b49780caeb9.herokuapp.com/'
+  ? new Sequelize(process.env.'https://shrouded-sierra-56762-1b49780caeb9.herokuapp.com/)' // https://shrouded-sierra-56762-1b49780caeb9.herokuapp.com/ is for HEROKU
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-      host: 'localhost',
+      host: 'http://localhost:3001/',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
